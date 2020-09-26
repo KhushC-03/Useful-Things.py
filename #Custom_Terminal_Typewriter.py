1e26f28217adc63f5c7fab885b3d1d32561f4710
KhushC-03 <68359616+KhushC-03@users.noreply.github.com>
2020-09-26 16:16:06 +0100

    Added EXIT feature

--- #Custom_Terminal_Typewriter.py
+++ #Custom_Terminal_Typewriter.py
@@ -9,16 +9,19 @@ PURPLE = '\x1b[1;35m'
 CYAN = '\x1b[1;36m'
 WHITE = '\x1b[1;37m'
 import os
+print("Type 'exit' to leave") 
 os.system('cls')
 def writer():
-    I1 = input(WHITE)
-    document = open('yourfilepath+\\document.txt','a')
-    I2 = '{}\n'.format(I1)
-    document.write(I2)
-    document.close()
-    writer()
+    I1 = input()
+    if I1 == 'exit':
+        os.system('exit')
+    else:
+        document = open('C:\\Users\\Khush Chauhan\\Desktop\\GitHubRepo\\document.txt','a')
+        I2 = '{}\n'.format(I1)
+        document.write(I2)
+        document.close()
+        writer()
 try:
     writer()
 except Exception as c:
     print(c)
-"""CLICK CTRL + C TO FINISH TYPING"""
